b4248f3c60bc1e3574609cb6add82012c11a5174
Nguyen Nga <ngant1096@gmail.com>
2019-09-24 11:55:10 +0700

    base project

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+ return [
+    'CONTENT_PERMISSION' => 'content_manage',
+    'USER_PERMISSION' => 'users_manage',
+    'THEME_PERMISSION' => 'theme_manage',
+    'ORDER_PERMISSION' => 'order_manage',
+    'DEVICE_PERMISSION' => 'device_manage'
+
+ ];
